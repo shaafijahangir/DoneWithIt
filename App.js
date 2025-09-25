a3b4ee1463cc12858190ff98771b8dae9656d665
M Shaafi Jahangir <97818517+shaafijahangir@users.noreply.github.com>
2025-09-25 01:28:25 -0700

    Initial commit for native-react DoneWithIt project

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("app launched");
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
